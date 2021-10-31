760c0cfffd21a409d98421afda982daee454a8c4
aqlzh <lzh_create@qq.com>
2021-10-31 22:36:48 +0800

    feat:21/10/31

---
+++ MyCode/1.实验一/1.编译原理_实验一.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+// ´Ê·¨´íÎó²âÊÔ
+
+void test31() {
+	//intt a = 10;    // 1¡¢¶àÓà×Ö·û
+	//in b = 10;     // 2¡¢ÒÅÂ©µÄ×Ö·û
+	//1nt c = 10;    // 3¡¢´úÌæ×Ö·û
+	//int 10 = d;   // 4¡¢½»»»×Ö·û
+	//int e = 2     // 5¡¢·ÖºÅ
+	//int f = 5 / 0;   //6¡¢Óï·¨
+}
+
+
+
+int main01() {
+	
+	test31();
+	system("pause");
+	return 0;
+}
+
+
+
+
+
